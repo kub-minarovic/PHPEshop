b52e27c50b447bd79be336d59aecb68f7a4d6070
jacobm44 <jacobm44@gmail.com>
2014-03-14 09:47:26 +0100

    Config

--- goldwakeYii/protected/config/main.php
+++ goldwakeYii/protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'GODLWAKW Surf Shop',
+	'name'=>'GODLWAKE Surf Shop',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
